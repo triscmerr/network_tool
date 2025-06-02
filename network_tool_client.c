a519879f3c3e691040361852682b116a6e3343d4
tricmer <merrillt@laptop-lenovo-tristan.localdomain>
2025-06-02 15:38:22 +0200

    Test commit

---
+++ network_tool_client.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <openssl/evp.h>
+
+#define MSG_SZE 100
+#define CIPHER_TXT_LEN 128
+
+void encrypt(char* msg, char* cipher_txt){
+
+    char key[32] = "0123456789qwertyuiopasdfghjklzxc";
+    char iv[16] = "0123456789abcdef";
+    int cipher_txt_len = CIPHER_TXT_LEN;
+
+    EVP_CIPHER_CTX *ctx;
+ctx = EVP_CIPHER_CTX_new();
+if (!ctx){
+    perror("Error creating cipher context\n");
+    exit(EXIT_FAILURE);
+}
+
+if (EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv) != 1){
+
+    perror("Error initalizing the cipher\n");
+    exit(EXIT_FAILURE);
+}
+
+if(EVP_EncryptUpdate(ctx, cipher_txt, &cipher_txt_len, msg, (int)strlen(msg) ) != 1){
+
+    perror("Error encrypting");
+    exit(EXIT_FAILURE);
+}
+
+if (EVP_EncryptFinal_ex(ctx, cipher_txt, &cipher_txt_len) != 1){
+
+    perror("Error encrypting");
+    exit(EXIT_FAILURE);
+}
+
+}
+
+int communicate(char* msg){
+
+    char cipher_txt[CIPHER_TXT_LEN];
+    printf("%s", msg);
+
+    int sockfd;
+    //create a socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //connect to remote server with socket
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(7777);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr); //convert IP to binary format
+
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    encrypt(msg, cipher_txt);
+    printf("Encrypted message %s as %s\n", msg, cipher_txt);
+    //send message to remote server
+    int bytes_sent = 0;
+    while(bytes_sent < MSG_SZE){
+        
+        printf("Sending: %s\n", cipher_txt+bytes_sent);
+        
+        bytes_sent+=send(sockfd, cipher_txt+bytes_sent, MSG_SZE-bytes_sent, 0);
+        printf("sent: %d bytes of %d bytes sent\n", bytes_sent, MSG_SZE);
+
+    }
+
+    
+    recv(sockfd, msg, MSG_SZE, 0);
+   
+
+    printf("New received message: %s\n", msg);
+
+
+    return 0;
+}
+
+int main(char ** argv, int argc){
+
+char input[100];
+printf("Enter string\n");
+fgets(input, sizeof(input), stdin);
+
+
+communicate(input);
+
+
+    return 0;
+}
---
+++ network_tool_server.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <openssl/evp.h>
+
+void decrypt(char* plain_txt, char* cipher_txt){
+
+    char key[32] = "0123456789qwertyuiopasdfghjklzxc";
+    char iv[16] = "0123456789abcdef";
+
+    int plain_txt_len = strlen(cipher_txt);
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    if (!ctx){
+    perror("Error creating cipher context\n");
+    exit(EXIT_FAILURE);
+}
+    
+    EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+
+    EVP_DecryptUpdate(ctx, plain_txt, &plain_txt_len, cipher_txt, strlen(cipher_txt));
+
+    EVP_DecryptFinal_ex(ctx, plain_txt, &plain_txt_len);
+
+
+}
+
+
+int run_server(){
+
+char buffer [1024];
+char buffer_decrypted[1024];
+
+int sockfd;
+sockfd = socket(AF_INET, SOCK_STREAM, 0);
+if (sockfd == -1) {
+    perror("Socket creation failed");
+    exit(EXIT_FAILURE);
+}
+
+
+struct sockaddr_in server_addr;
+server_addr.sin_family = AF_INET;
+server_addr.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP
+server_addr.sin_port = htons(7777); // Port number
+
+if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    perror("Bind failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+}
+
+if (listen(sockfd, 5) == -1) { // Allow up to 5 pending connections
+    perror("Listen failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+}
+       
+
+int client_sock;
+struct sockaddr_in client_addr;
+socklen_t client_len = sizeof(client_addr);
+
+while(1) {
+client_sock = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
+if (client_sock == -1) {
+    perror("Accept failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+}
+
+memset(buffer, 0, sizeof(buffer));
+recv(client_sock, buffer, sizeof(buffer), 0);   
+decrypt(buffer_decrypted, buffer);
+
+char decrypt_msg [100];
+strncpy(decrypt_msg, buffer_decrypted, 100);
+printf("Here is the received string: %s - decrypted as: %s\n", buffer, buffer_decrypted);
+
+for(int i =0; i < sizeof(buffer_decrypted); i++){
+
+    buffer_decrypted[i] = buffer_decrypted[i]+1;
+}
+
+int bytes_sent = 0;
+while (bytes_sent < sizeof(buffer_decrypted)){
+bytes_sent+=send(client_sock, buffer_decrypted, sizeof(buffer_decrypted), 0) < sizeof(buffer_decrypted);
+
+}
+
+}
+
+    return 0;
+}
+
+int main() {
+
+run_server();
+
+    return 0;
+}
